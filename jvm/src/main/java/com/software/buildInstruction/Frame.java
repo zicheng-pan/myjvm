8799975d36efde84fce32b25127d397f0f19f989
zicheng.pan <1094275633@qq.com>
2021-11-24 18:01:57 +0800

    add more comment

--- jvm/src/main/java/com/software/buildInstruction/Frame.java
+++ jvm/src/main/java/com/software/buildInstruction/Frame.java
@@ -7,13 +7,14 @@ import java.util.Stack;
 //栈帧 对象
 public class Frame {
 
-    //程序计数器
+    //程序计数器 没有OOM的问题
     public int pc = 0;
 
-    //本地变量表
+    //本地变量表  用来存放 this变量->方法中的其他形参->方法内部定义的局部变量
     public final Map<Integer, Integer> localVars = new HashMap<Integer, Integer>();
 
-    //操作数栈
+    //操作数栈  主要用于保存计算过程的中间结果，同时作为计算过程中变量临时的存储空间。
+    //操作数栈就是JVM执行引擎的一个工作区，当一个方法刚开始执行的时候，一个新的栈帧也会随之被创建出来，这时方法的操作数栈是空的（这个时候数组是有长度的，只是操作数栈为空）
     public final Stack<Integer> operandStack = new Stack<Integer>();
 
 }
--- jvm/src/main/java/com/software/buildInstruction/Interpreter.java
+++ jvm/src/main/java/com/software/buildInstruction/Interpreter.java
@@ -2,7 +2,7 @@ package com.software.buildInstruction;
 
 import java.util.Map;
 
-//解释器
+//解释器 执行指令的集合 可以将class文件翻译成指定的指令，然后进行执行
 public class Interpreter {
     public static void run(Frame frame, Map<Integer, Instruct> instructions) {
         do {
